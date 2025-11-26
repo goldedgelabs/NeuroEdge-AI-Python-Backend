7cf8ea5ee30500874daf9f67dab735e2096c3600
goldedgelabs <goldedgelabs@gmail.com>
2025-11-26 14:56:05 +0300

    Update eventBus.py

--- backend-python/core/eventBus.py
+++ backend-python/core/eventBus.py
@@ -1,18 +1,23 @@
 # backend-python/core/eventBus.py
-
 import asyncio
+from collections import defaultdict
+from utils.logger import logger
 
 class EventBus:
     def __init__(self):
-        self.subscribers = {}
+        self.subscribers = defaultdict(list)
 
-    def subscribe(self, event_name: str, callback):
-        if event_name not in self.subscribers:
-            self.subscribers[event_name] = []
-        self.subscribers[event_name].append(callback)
+    def subscribe(self, event_type: str, callback):
+        self.subscribers[event_type].append(callback)
+        logger.log(f"[EventBus] Subscribed to event: {event_type}")
 
-    def publish(self, event_name: str, data):
-        for callback in self.subscribers.get(event_name, []):
-            asyncio.create_task(callback(data))
+    def publish(self, event_type: str, data: dict):
+        logger.log(f"[EventBus] Publishing event: {event_type} → {data.get('collection', '')}:{data.get('key', '')}")
+        for callback in self.subscribers[event_type]:
+            # Call asynchronously if coroutine
+            if asyncio.iscoroutinefunction(callback):
+                asyncio.create_task(callback(data))
+            else:
+                callback(data)
 
 eventBus = EventBus()
