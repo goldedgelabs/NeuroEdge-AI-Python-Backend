85d6f14a1f79a4ba63f0ba57c23dc5e52b3fefc4
goldedgelabs <goldedgelabs@gmail.com>
2025-11-26 14:34:17 +0300

    Update eventBus.py

--- backend-python/core/eventBus.py
+++ backend-python/core/eventBus.py
@@ -1,19 +1,18 @@
 # backend-python/core/eventBus.py
 
+import asyncio
+
 class EventBus:
     def __init__(self):
-        self.listeners = {}
+        self.subscribers = {}
 
     def subscribe(self, event_name: str, callback):
-        if event_name not in self.listeners:
-            self.listeners[event_name] = []
-        self.listeners[event_name].append(callback)
+        if event_name not in self.subscribers:
+            self.subscribers[event_name] = []
+        self.subscribers[event_name].append(callback)
 
-    def publish(self, event_name: str, data: dict):
-        for callback in self.listeners.get(event_name, []):
-            # schedule asynchronously
-            import asyncio
+    def publish(self, event_name: str, data):
+        for callback in self.subscribers.get(event_name, []):
             asyncio.create_task(callback(data))
 
-# Singleton instance
 eventBus = EventBus()
